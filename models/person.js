00982c7852c937612e6c293a847143ddefd99a51
Bernard De Guzman <bernard.deguzman@technomancer.biz>
2023-12-28 20:15:46 +0800

    Add logs for connecting

--- models/person.js
+++ models/person.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false);
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url)
-
+mongoose
+    .connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+    
 const personSchema = new mongoose.Schema({
     name: String,
     number: String
